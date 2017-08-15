09ad7730cec5cfb2ee74a8ce88d32617172fb76a
Arvid <arvid.bild@gmail.com>
2017-08-15 12:17:18 +0200

    just changed the structure

--- src/models/User.js
+++ src/models/User.js
@@ -5,26 +5,12 @@ var Schema = mongoose.Schema;
 
 var userSchema = new Schema ({
     
-    fullName: {
-        type: String,
-    required: [true, "you have to provide a full name"] 
-    },
-    
- emailAdress: {
-       type: String,
-   required: [true, "you have to provide an email"],
-     unique: [true, "Email belongs to a existing user"]
-    },
-    
-   imagePath: {
-        type: String,
-      },
-    assets: {
-        type: String,
-    }
-    
-        
+    fullName: {type: String, required: [true, "you have to provide a full name"]},
+ emailAdress: {type: String, required: [true, "you have to provide an email"], unique: [true, "Email belongs to a existing user"]},  
+   imagePath: {type: String},
+      assets: {type: String}        
 });
 
-var user = mongoose.model("user", userSchema);
-module.exports = user;
+
+module.exports = mongoose.model("User", userSchema);
+ 
