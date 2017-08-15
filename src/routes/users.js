318a95aa3bcfaa130d9e4da2b586daea073fa1e8
Arvid <arvid.bild@gmail.com>
2017-08-15 12:18:01 +0200

    created the profile route

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var passport = require("passport");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -9,5 +10,21 @@ router.get('/', function(req, res, next) {
 router.get("/signup", function(req,res,next){
 	res.render("signup", null);
 }); 
+
+
+router.get('/auth/facebook',
+  passport.authenticate('facebook'));
+
+router.get('/auth/facebook/callback',
+  passport.authenticate('facebook', { failureRedirect: '/' }), function(req, res) {
+  	console.log("the authentication was Successful");
+    
+    // Successful authentication, redirect to profile.
+    res.redirect('/users/profile');
+  });
+
+router.get("/profile", function(req,res,next){
+	res.render("profile");
+});
 
 module.exports = router;
