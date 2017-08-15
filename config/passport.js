03fa92e589f905d851ff93cef12e983dbb75a184
Arvid <arvid.bild@gmail.com>
2017-08-15 11:06:36 +0200

    added code in passport.js file

---
+++ config/passport.js
@@ -0,0 +1,34 @@
+var passport = require('passport');
+var User = require('../src/models/User');
+var LocalStrategy = require('passport-local').Strategy;
+var FacebookStrategy = require("passport-facebook").Strategy;
+
+
+//use facebook login with passport strategy (process.env. is enviroment variables)
+passport.use(new FacebookStrategy({
+    clientID: "1564331986944420",
+    clientSecret: "481bc9b42684dd0431322e0d7eead698",
+    callbackURL: "http://localhost:3000/user/auth/facebook/callback",
+    profileFields: ['id', 'displayName', 'link', 'photos', 'emails']
+  },
+    function (accessToken, refreshToken, profile, done){
+        console.log(profile);
+      if(profile.emails[0]) {
+        User.findOneAndUpdate(
+          { email: profile.emails[0].value },
+          {
+            name: profile.displayName || profile.username,
+            email: profile.emails[0].value,
+            photo: profile.photos[0].value
+          },
+          {
+            upsert: true
+          },
+        done
+      );
+      } else {
+        var noEmailError = new Error("Your email privacy settings prevent you from signing into Bookworm.");
+        done(noEmailError, null);
+      }
+    }
+));
